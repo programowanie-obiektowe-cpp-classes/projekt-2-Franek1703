7f642f3525b4e7e71224449763637149624372de
FRANCISZEK ZAREBSKI <fran.zarebski@gmail.com>
2025-01-23 17:35:15 +0100

    Add day1 part 2

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,28 +1,56 @@
 #include "day1.hpp"
 #include "input_parser.hpp"
-#include <iostream>
-#include <vector>
 #include <algorithm>
 #include <cmath>
 #include <fstream>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
 
 /// Function to compute the total distance
-int computeTotalDistance(std::vector<int>& leftList, std::vector<int>& rightList) {
+int computeTotalDistance(std::vector< int >& leftList, std::vector< int >& rightList)
+{
     std::sort(leftList.begin(), leftList.end());
     std::sort(rightList.begin(), rightList.end());
 
     int totalDistance = 0;
 
-    for (size_t i = 0; i < leftList.size(); ++i) {
+    for (size_t i = 0; i < leftList.size(); ++i)
+    {
         totalDistance += std::abs(leftList[i] - rightList[i]);
     }
 
     return totalDistance;
 }
 
+int calculateSimilarityScore(const std::vector< int >& leftList, const std::vector< int >& rightList)
+{
+    std::unordered_map< int, int > rightListCounts;
+
+    // Count the occurrences of each number in the right list
+    for (int num : rightList)
+    {
+        rightListCounts[num]++;
+    }
+
+    int similarityScore = 0;
+
+    // Calculate the similarity score by multiplying the number and its count in the right list 2D array
+    for (int num : leftList)
+    {
+        if (rightListCounts.find(num) != rightListCounts.end())
+        {
+            similarityScore += num * rightListCounts[num];
+        }
+    }
+
+    return similarityScore;
+}
+
 // Function to solve Day 1
-void solveDay1(const std::string& inputFile) {
-    std::vector<int> leftList, rightList;
+void solveDay1(const std::string& inputFile)
+{
+    std::vector< int > leftList, rightList;
 
     // Parse input
     InputParser::parseInput(inputFile, leftList, rightList);
@@ -32,4 +60,8 @@ void solveDay1(const std::string& inputFile) {
 
     // Output the result
     std::cout << "Total distance: " << totalDistance << std::endl;
+
+    int similarityScore = calculateSimilarityScore(leftList, rightList);
+
+    std::cout << "Similarity Score: " << similarityScore << std::endl;
 }
